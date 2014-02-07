b2b092dcadfc363c8d31d621074754f7c53486f5
cjclarke <chrisclarke@outlook.com>
2014-02-07 18:04:37 +0000

    WallFollower 1A2

--- src/WallFollower.java
+++ src/WallFollower.java
@@ -5,17 +5,52 @@ import lejos.nxt.Motor;
 public class WallFollower
 {
 	public static void main (String[] args){
+		LCD.drawString("Motor Test: Forward",0,0);
+		Forward(150);
+		LCD.drawString("Motor Test: Backward",0,0);
+		Backward(150);
+		LCD.drawString("Motor Test: Left",0,0);
+		Left(150);
+		LCD.drawString("Motor Test: Right",0,0);
+		Right(150);
+		Button.waitForAnyPress();
+	}
+	
+	public static void Forward(int time){
 		try {
-		LCD.drawString("Hello World",0,0);
-		Button.waitForAnyPress();
+		Motor.A.forward();
 		Motor.B.forward();
-		LCD.drawString("Went Forward",0,0);
-		Thread.sleep(500);
-		Motor.B.stop();
-		LCD.drawString("Stopped",0,0);
-		Thread.sleep(500);
-		Button.waitForAnyPress();
-		}
+		Motor.C.forward();
+		Thread.sleep(time);
+				}
 		catch (Exception e) {}
+	}
+	
+	public static void Backward(int time){
+		try {
+		Motor.A.backward();
+		Motor.B.backward();
+		Motor.C.backward();
+		Thread.sleep(time);
+				}
+		catch (Exception e) {}
+	}
+	
+	public static void Left(int time){
+		try {
+		Motor.A.backward();
+		Motor.B.forward();
+		Motor.C.forward();
+		Thread.sleep(time);
+		} catch (Exception e) {}
+	}
+	
+	public static void Right(int time){
+		try {
+		Motor.A.forward();
+		Motor.B.backward();
+		Motor.C.forward();
+		Thread.sleep(time);
+		} catch (Exception e) {}
 	}
 }
