cc58b3b95f6535224afae21792aab065dc7b8571
birch <akb29@bath.ac.uk>
2014-02-11 16:49:24 +0000

    sensors

--- src/WallHugger.java
+++ src/WallHugger.java
@@ -1,6 +1,8 @@
 import lejos.nxt.Button;
 import lejos.nxt.LCD;
-import lejos.nxt.LightSensor;
+//import lejos.nxt.LightSensor;
+import lejos.nxt.UltrasonicSensor;
+import lejos.nxt.TouchSensor;
 import lejos.nxt.SensorPort;
 
 /**
@@ -12,11 +14,15 @@ public class WallHugger {
 	final static int INTERVAL = 200; // milliseconds
 	
 	public static void main(String [] args) throws Exception {
-		LightSensor cmps = new LightSensor(SensorPort.S1);
+		//LightSensor cmps = new LightSensor(SensorPort.S1);
+		UltrasonicSensor sonic = new UltrasonicSensor(SensorPort.S1);
+		TouchSensor touch = new TouchSensor(SensorPort.S2);
 		
 		while(!Button.ESCAPE.isDown()) {
 			LCD.clear();
-			LCD.drawInt(cmps.readValue(),7,3);
+			//LCD.drawInt(sonic.readValue(),7,3);
+			System.out.println("touch = " + touch.isPressed());
+			System.out.println("distance = " + sonic.getDistance());
 			LCD.refresh();
 			Thread.sleep(INTERVAL);
 		}
