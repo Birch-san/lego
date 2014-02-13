9e75c95625fe1db08fe4f2b7ceb8c01755128a75
birch <akb29@bath.ac.uk>
2014-02-13 11:14:40 +0000

    twerking around

--- src/WallHugger.java
+++ src/WallHugger.java
@@ -5,6 +5,10 @@ import lejos.nxt.UltrasonicSensor;
 import lejos.nxt.TouchSensor;
 import lejos.nxt.SensorPort;
 
+import java.io.File;
+
+import lejos.nxt.Sound;
+
 /**
  * For testing the HiTechnic color sensor (see lejos.nxt.addon.ColorHTSensor).
  * @author BB
@@ -16,14 +20,26 @@ public class WallHugger {
 	public static void main(String [] args) throws Exception {
 		//LightSensor cmps = new LightSensor(SensorPort.S1);
 		UltrasonicSensor sonic = new UltrasonicSensor(SensorPort.S1);
-		TouchSensor touch = new TouchSensor(SensorPort.S2);
+		TouchSensor touchFront = new TouchSensor(SensorPort.S3);
+		
+		File file1 = new File("OwLoud.wav");
+
+		Thread.sleep(2000);
+		
+		//File file2 = new File("PetrolLoud.wav");
+		//Sound.playSample(file2, 100);
+
 		
 		while(!Button.ESCAPE.isDown()) {
 			LCD.clear();
 			//LCD.drawInt(sonic.readValue(),7,3);
-			System.out.println("touch = " + touch.isPressed());
-			System.out.println("distance = " + sonic.getDistance());
-			LCD.refresh();
+			//System.out.println("touch = " + touchFront.isPressed());
+			//System.out.println("distance = " + sonic.getDistance());
+			//LCD.refresh();
+			WallFollower.Right(INTERVAL);
+			if (Sound.getTime() < 1) {
+				Sound.playSample(file1, 200);
+			}
 			Thread.sleep(INTERVAL);
 		}
 	}
